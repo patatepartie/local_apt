1e83f2562ba1c5a7760ce7e70e892cc2fb80e416
Cyril Ledru <cyril.ledru@kvh.co.jp>
2014-04-24 11:25:40 +0900

    Fix metadata license
    Remove the update custom matcher.

--- libraries/matchers.rb
+++ libraries/matchers.rb
@@ -20,10 +20,6 @@ if defined?(ChefSpec)
     ChefSpec::Matchers::ResourceMatcher.new(:local_apt_repository, :add, repository_name)
   end
 
-  def update_local_apt_repository(repository_name)
-    ChefSpec::Matchers::ResourceMatcher.new(:local_apt_repository, :update, repository_name)
-  end
-
   def remove_local_apt_repository(repository_name)
     ChefSpec::Matchers::ResourceMatcher.new(:local_apt_repository, :remove, repository_name)
   end
--- metadata.rb
+++ metadata.rb
@@ -1,10 +1,10 @@
 name             'local_apt'
 maintainer       'Cyril Ledru'
 maintainer_email 'cyril.ledru@gmail.com'
-license          'All rights reserved'
-description      'Installs/Configures local_apt'
+license          'Apache 2.0'
+description      'Installs/Configures local apt repositories'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.2.0'
-supports 'ubuntu'
+version          '0.1.1'
+supports         'ubuntu'
 
 depends 'apt'
